5702f34af4ddbf5321dbe8b02ac2ca3a919dc8d3
KohiOfficial <50747401+KohiOfficial@users.noreply.github.com>
2019-05-20 19:12:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -155,6 +155,34 @@ bot.on('message', message=>{
             default:
             message.channel.send("That command doesn't exist / Böyle bir komut yok")
             break;
+            // This command requires the discord-js package to create embeds
+const Discord = require('discord.js');
+
+exports.run = async (client, message, args, tools) => {
+
+    // Role Verification (Optional)
+    if (!message.member.roles.find(r => r.name === 'roleName')) return message.channel.send('This requires the role: roleName');
+
+    // Permission Verification (Optional)
+    if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('This requires the permission: ADMINISTRATOR');
+
+    if (!args[0]) return message.channel.send('Proper Usage: <prefix>poll question');
+
+    const embed = new Discord.RichEmbed()
+        .setColor(0xffffff)
+        .setFooter('React to vote.')
+        .setDescription(args.join(' '))
+        .setTitle(`Poll created by ${message.author.username}`);
+
+    let msg = await message.channel.send(embed);
+
+    await msg.react('✅'); 
+    await msg.react('❌');
+
+    message.delete({timeout: 1000}); // This waits 1000 milliseconds before deleting (1 second)
+
+} 
+
 
             
     
