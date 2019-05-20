c168efef06348a4eb7d2edbb6771c1af603c069c
KohiOfficial <50747401+KohiOfficial@users.noreply.github.com>
2019-05-20 18:25:35 +0300

    Update poll.js

--- poll.js
+++ poll.js
@@ -1,35 +1,27 @@
-// This commands requires the discord.js to create embeds
+// This command requires the discord-js package to create embeds
 const Discord = require('discord.js');
 
 exports.run = async (client, message, args, tools) => {
-    
-    
-    // This will contain some extra things
-    
-    // Role Verification -- This will run only if a user has a certain role (optional)
+
+    // Role Verification (Optional)
     if (!message.member.roles.find(r => r.name === 'roleName')) return message.channel.send('This requires the role: roleName');
-    
-    // Permission Verification -- This will run only if a user has a certain permission (optional)
+
+    // Permission Verification (Optional)
     if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('This requires the permission: ADMINISTRATOR');
-    
-    // First, we want to check if user had a input
+
     if (!args[0]) return message.channel.send('Proper Usage: <prefix>poll question');
-    
-    // Then, create the embed
+
     const embed = new Discord.RichEmbed()
         .setColor(0xffffff)
         .setFooter('React to vote.')
         .setDescription(args.join(' '))
         .setTitle(`Poll created by ${message.author.username}`);
-    // Finally, using await send the message
-    let msg = await message.channel.send(embed);
-    // The sent message will now be stored in the msg variable
-    
-    // React to the message
-    await msg.react('\:white_check_mark:'); // Using await here will make sure the first one runs before second
-    await msg.react('\:no_entry:');
-    
-   // Make sure you delete the original message 
-    message.delete({timeout: 1000});
 
-}
+    let msg = await message.channel.send(embed);
+
+    await msg.react('✅'); 
+    await msg.react('❌');
+
+    message.delete({timeout: 1000}); // This waits 1000 milliseconds before deleting (1 second)
+
+} 
